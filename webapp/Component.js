239c33c677dc766b5a8db3ff8fb4e88f220b7942
gaode2024 <gaode2019@gmail.com>
2019-09-06 13:20:06 +0800

    oo

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("supplierssuppliers_list.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+jQuery.sap.require("sap.m.MessageToast");
+sap.ui.generic.app.AppComponent.extend("supplierssuppliers_list.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
---
+++ webapp/ext/controller/Message.controller.js
@@ -0,0 +1,9 @@
+sap.ui.controller("supplierssuppliers_list.ext.controller.Message", {
+	onInit: function() {},
+	onExit: function() {},
+	onBeforeRendering: function() {},
+	onAfterRendering: function() {},
+	onMessage: function(oEvent){
+		sap.m.MessageToast.show("OK!");
+	}
+});
